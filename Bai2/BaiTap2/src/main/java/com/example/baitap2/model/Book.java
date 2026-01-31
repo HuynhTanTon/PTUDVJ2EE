495ac6633c6ff0f3d5c2bb207fe1e1c28d71c724
HuynhTanTon <email_cua_ban@gmail.com>
2026-01-31 13:19:28 +0700

    sắp xếp lại bài tập

---
+++ Bai2/BaiTap2/src/main/java/com/example/baitap2/model/Book.java
@@ -0,0 +1,18 @@
+package com.example.baitap2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+}
